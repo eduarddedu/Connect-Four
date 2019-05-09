eduarddedu/Connect-Four

commit e8ef57f26d1640a59ff8ab80ae8db85dc75e7ba1

Cleanup

src/app/cookie.service.ts
@@ -1,28 +1,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class CookieService {
-
-  public setCookie(cname: string, cvalue: string, exdays: number) {
-    const date = new Date();
-    date.setTime(date.getTime() + exdays * 24 * 60 * 60 * 1000);
-    const expires = 'expires=' + date.toUTCString();
-    document.cookie = cname + '=' + cvalue + ';' + expires + ';' + 'path=/';
-  }
-
-  public getCookie(cname: string): string {
-    const name = cname + '=';
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-      const c = ca[i].replace(/^\s+/gm, '');
-      if (c.indexOf(name) === 0) {
-        return c.substr(name.length);
-      }
-    }
-    return '';
-  }
-
-}

src/app/home/home.component.html
@@ -18,11 +18,9 @@
   <main>
     <div class="box">
       <app-notification></app-notification>
-      <section id="panels" *ngIf="user">
-        <app-panel-players [user]="user" (joinGame)="joinGame($event)" [panelVisible]="panelsVisible">
-        </app-panel-players>
-        <app-panel-games (joinGame)="joinGame($event)" [panelVisible]="panelsVisible">
-        </app-panel-games>
+      <section id="panels" *ngIf="showPanels">
+        <app-panel-players [user]="user" (joinGame)="joinGame($event)"></app-panel-players>
+        <app-panel-games (joinGame)="joinGame($event)"></app-panel-games>
       </section>
       <router-outlet (activate)="onGameLoad($event)"></router-outlet>
     </div>

src/app/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
   gameCompRef: GameComponent;
   user: User;
   showDropdownMenu = false;
-  private panelsVisible = true;
+  showPanels = false;
   private ds: deepstreamIO.Client;
 
   constructor(private router: Router,
@@ -31,7 +31,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     if (this.auth.user) {
       this.user = this.auth.user;
-      this.panelsVisible = true;
+      this.showPanels = true;
       this.ds = this.deepstreamService.getInstance();
       this.ds.record.getList('games').on('entry-removed', this.onGameRecordDelete.bind(this));
       this.ds.record.getList('users').whenReady(list => {
@@ -52,7 +52,7 @@ export class HomeComponent implements OnInit {
       this.getGameQuitOption();
     } else {
       this.router.navigate(['/']);
-      this.panelsVisible = true;
+      this.showPanels = true;
       this.gameCompRef = null;
     }
   }
@@ -65,7 +65,7 @@ export class HomeComponent implements OnInit {
         this.ds.record.getRecord(this.gameCompRef.opponent.id).set('status', 'Online');
         this.ds.record.getList('games').removeEntry(this.gameCompRef.game.id);
         this.ds.record.getRecord(this.gameCompRef.game.id).delete();
-        this.panelsVisible = true;
+        this.showPanels = true;
         this.ngZone.run(() => this.router.navigate(['/']));
       }
     });
@@ -76,7 +76,7 @@ export class HomeComponent implements OnInit {
   }
 
   joinGame(gameId: string) {
-    this.panelsVisible = false;
+    this.showPanels = false;
     this.router.navigate([`/game/${gameId}`]);
   }
 

src/app/game/game-model.ts
@@ -314,7 +314,7 @@ e c e e
 e c e c
 c e c c
 
-...etc are all valid arrays, assuming c referes to the same color, red or yellow.
+...etc are all valid arrays, assuming c refers to the same color, red or yellow.
 
 While an array of four colored cells fetches a score of Infinity, lesser arrays are scored based on powers of ten.
 E.g., a array with one colored cell fetches 20 x 10^0, a array with two colored cells fetches 20 x 10^1.

src/app/panels/panel-games/panel-games.component.html
@@ -1,4 +1,4 @@
-<div class="c4-card" *ngIf="panelVisible">
+<div class="c4-card">
   <h6 class="card-header text-center">Watch</h6>
   <p *ngIf="games.length == 0">No games yet</p>
   <table>

src/app/panels/panel-games/panel-games.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
 
 import { Game } from '../../game/game';
 import { DeepstreamService } from '../../deepstream.service';
@@ -9,10 +9,9 @@ import { DeepstreamService } from '../../deepstream.service';
   styleUrls: ['./panel-games.component.css', '../panels-styles.css']
 })
 export class PanelGamesComponent implements OnInit {
-  @Input() panelVisible = true;
   @Output() joinGame: EventEmitter<string> = new EventEmitter();
+  games: Game[] = [];
   private deepstream: deepstreamIO.Client;
-  private games: any[] = [];
 
   constructor(private cdr: ChangeDetectorRef, ds: DeepstreamService) {
     this.deepstream = ds.getInstance();

src/app/panels/panel-players/panel-players.component.html
@@ -1,4 +1,4 @@
-<div class="c4-card" *ngIf="panelVisible">
+<div class="c4-card">
   <h6 class="card-header text-center">Invite</h6>
   <p *ngIf="players.size == 0">No one is online</p>
   <table>

src/app/panels/panel-players/panel-players.component.ts
@@ -30,9 +30,8 @@ export interface Invitation {
 
 export class PanelPlayersComponent implements OnInit {
   @Input() user: User;
-  @Input() panelVisible = true;
   @Output() joinGame: EventEmitter<string> = new EventEmitter();
-  private players: Map<string, User> = new Map();
+  players: Map<string, User> = new Map();
   private botUser: User = {
     id: '0',
     name: 'Bobiță',

commit 862c72dbb9a698f128b64e1ec54461b00d569273

Feature: policy page/accept + fix bug introduced in e8ef57f26

src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { GameComponent } from './game/game.component';
+import { PolicyPageComponent } from './policy-page/policy-page.component';
 
 const routes: Routes = [
   {
@@ -19,6 +20,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'policy',
+    component: PolicyPageComponent
   },
   {
     path: '**',

src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginModule } from './login/login.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PolicyAcceptComponent } from './policy-accept/policy-accept.component';
 import { HomeComponent } from './home/home.component';
 import { GameComponent } from './game/game.component';
 import { BoardComponent } from './game/board/board.component';
@@ -17,11 +18,14 @@ import { NotificationComponent } from './notification/notification.component';
 import { QuitGameComponent } from './modals/quit-game/quit-game.component';
 import { ParseFirstWordPipe } from './util/parseFirstWord';
 import * as _ from 'deepstream.io-client-js';
+import { PolicyPageComponent } from './policy-page/policy-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    PolicyAcceptComponent,
     HomeComponent,
     GameComponent,
     BoardComponent,
@@ -32,14 +36,14 @@ import * as _ from 'deepstream.io-client-js';
     GameInvitationComponent,
     NotificationComponent,
     QuitGameComponent,
-    ParseFirstWordPipe
+    ParseFirstWordPipe,
+    PolicyPageComponent
   ],
   imports: [
     BrowserModule,
     NgbModalModule,
     NgbAlertModule,
-    AppRoutingModule,
-    LoginModule
+    AppRoutingModule
   ],
   providers: [],
   entryComponents: [GameInvitationComponent, InvitationRejectedComponent, QuitGameComponent],

src/app/auth.service.ts
@@ -65,8 +65,7 @@ export class AuthService {
         if (this.GoogleAuth.isSignedIn.get() === true) {
           user.next(getUser(this.GoogleAuth.currentUser.get()));
         } else {
-          const button: Element = document.querySelector('.g-login-btn');
-          this.GoogleAuth.attachClickHandler(button, {}, (googleUser: any) => user.next(getUser(googleUser)));
+          this.GoogleAuth.attachClickHandler('g-login-btn', {}, (googleUser: any) => user.next(getUser(googleUser)));
         }
       }, (error: any) => {
         console.log(`${error.error} ${error.details}`);

src/app/cookie.service.ts
@@ -1,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CookieService {
+
+  public setCookie(cname: string, cvalue: string, exdays: number) {
+    let cookie = `${cname}=${cvalue};`;
+    const attrs: Map<string, string> = new Map();
+    attrs.set('domain', environment.origin);
+    attrs.set('path', '/');
+    attrs.set('expires', new Date(Date.now() + exdays * 24 * 60 * 60 * 1000).toUTCString());
+    attrs.forEach((value: string, key: string) => cookie += `${key}=${value};`);
+    document.cookie = cookie;
+  }
+
+  public getCookieValue(cname: string): string {
+    const name = cname + '=';
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      const c = ca[i].replace(/^\s+/gm, '');
+      if (c.indexOf(name) === 0) {
+        return c.substr(name.length);
+      }
+    }
+  }
+
+}

src/app/deepstream.service.ts
@@ -26,7 +26,7 @@ export class DeepstreamService {
 
   private init(user: User) {
     console.log('Deepstream init...');
-    this.deepstream = deepstream(environment.DEEPSTREAM_URL, { maxReconnectAttempts: 5 }).login({ username: user.name });
+    this.deepstream = deepstream(environment.deepstreamUrl, { maxReconnectAttempts: 5 }).login({ username: user.name });
     this.deepstream.on('error', (error: any, event: any, topic: any) => {
       console.log(error, event, topic);
     });

src/app/home/home.component.css
@@ -95,7 +95,7 @@ main {
         margin: 0 auto;
     }
 
-    #panels {
+    .panels-box {
         display: grid;
         grid-template-columns: 1fr 1fr;
         grid-gap: 10px;

src/app/home/home.component.html
@@ -1,9 +1,9 @@
-<div class="c4-container">
+<div class="c4-container" *ngIf="user">
   <header>
     <div class="brand" (click)="goHome()">
       <img src="assets/img/connect4.png" alt="board" role="navigation"> Connect Four
     </div>
-    <div class="profile" *ngIf="user">
+    <div class="profile">
       <div id="icon" class="icon" (click)="showPopoverMenu = !showPopoverMenu">
         <img [src]="user.iconUrl" alt>
       </div>
@@ -18,11 +18,14 @@
   <main>
     <div class="box">
       <app-notification></app-notification>
-      <section id="panels" *ngIf="showPanels">
-        <app-panel-players [user]="user" (joinGame)="joinGame($event)"></app-panel-players>
-        <app-panel-games (joinGame)="joinGame($event)"></app-panel-games>
+      <section id="panels" [ngStyle]="{'display' : showPanels ? 'initial' : 'none'}">
+        <div class="panels-box">
+          <app-panel-players [user]="user" (joinGame)="joinGame($event)"></app-panel-players>
+          <app-panel-games (joinGame)="joinGame($event)"></app-panel-games>
+        </div>
       </section>
       <router-outlet (activate)="onGameLoad($event)"></router-outlet>
     </div>
   </main>
+  <app-policy-accept></app-policy-accept>
 </div>

src/app/home/home.component.ts
@@ -16,7 +16,8 @@ import { GameComponent } from '../game/game.component';
 export class HomeComponent implements OnInit {
   gameCompRef: GameComponent;
   user: User;
-  showPanels = false;
+  showPanels = true;
+  showPopoverMenu = false;
   private ds: deepstreamIO.Client;
 
   constructor(private router: Router,
@@ -30,7 +31,6 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     if (this.auth.user) {
       this.user = this.auth.user;
-      this.showPanels = true;
       this.ds = this.deepstreamService.getInstance();
       this.ds.record.getList('games').on('entry-removed', this.onGameRecordDelete.bind(this));
       this.ds.record.getList('users').whenReady(list => {

src/app/login/login.component.css
@@ -21,7 +21,7 @@ main {
     height: 40px;
     width: 250px;
     border-radius: 2px;
-    margin-top: 8px;
+    margin-top: 12px;
 }
 
 .social-login-btn:hover {
@@ -63,7 +63,9 @@ main {
 
 .notice {
     margin-top: 8px;
-    max-width: 300px;
     font-size: .75em;
     text-align: center;
+}
+.notice p {
+    margin: 2px auto;
 }

src/app/login/login.component.html
@@ -4,8 +4,8 @@
   </header>
   <main>
     <div class="c4-card login-box">
-      <h5>Welcome to Connect Four!</h5>
-      <div class="social-login-btn g-login-btn">
+      <h5>Welcome stranger!</h5>
+      <div id="g-login-btn" class="social-login-btn g-login-btn">
         <span class="icon"></span>
         <span class="buttonText">Continue with Google</span>
       </div>
@@ -13,8 +13,11 @@
         <span class="icon"></span>
         <span class="buttonText">Continue with Facebook</span>
       </div>
-      <p class="notice">
-        By continuing you agree with our cookie <a href="#">policy.</a></p>
+      <div class="notice">
+        <p>To login with Facebook on a <b>Firefox</b> browser</p>
+        <p>you may need to turn <a href="https://support.mozilla.org/en-US/kb/content-blocking?as=u&utm_source=inproduct" target="_blank">content blocking</a>  off.</p>
+      </div>
     </div>
   </main>
+  <app-policy-accept></app-policy-accept>
 </div>

src/app/login/login.module.ts
@@ -1,17 +1,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './login.component';
-
-@NgModule({
-    declarations: [
-        LoginComponent
-    ],
-    imports: [
-        ReactiveFormsModule,
-        CommonModule
-    ],
-    providers: [],
-    bootstrap: [LoginComponent]
-})
-export class LoginModule { }

src/app/policy-accept/policy-accept.component.css
@@ -1,0 +1,10 @@
+footer {
+    padding: 16px 8px;
+    margin-top: 8px;
+    margin-bottom: -16px;
+    background-color: #e6b224;
+    color: black;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}

src/app/policy-accept/policy-accept.component.html
@@ -1,0 +1,8 @@
+<footer *ngIf="showComponent">
+  <div class="message">
+    By continuing, you agree to the terms of our <a routerLink="/policy">privacy policy</a>.
+  </div>
+  <div>
+    <button class="btn btn-light" (click)="onClickAcceptBtn()">Accept</button>
+  </div>
+</footer>

src/app/policy-accept/policy-accept.component.ts
@@ -1,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { CookieService } from '../cookie.service';
+
+@Component({
+  selector: 'app-policy-accept',
+  templateUrl: './policy-accept.component.html',
+  styleUrls: ['./policy-accept.component.css']
+})
+export class PolicyAcceptComponent implements OnInit {
+  showComponent = true;
+  readonly COOKIE_NAME = 'policy_accept';
+
+  constructor(private router: Router, private cookieService: CookieService) { }
+
+  ngOnInit() {
+    const policyCookie = this.cookieService.getCookieValue(this.COOKIE_NAME);
+    if (policyCookie) {
+      this.showComponent = false;
+    }
+  }
+
+  onClickAcceptBtn() {
+    this.cookieService.setCookie(this.COOKIE_NAME, 'true', 365);
+    this.showComponent = false;
+  }
+
+}

src/app/policy-page/policy-page.component.html
@@ -1,0 +1,3 @@
+<p>
+  policy-page works!
+</p>

src/app/policy-page/policy-page.component.ts
@@ -1,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-policy-page',
+  templateUrl: './policy-page.component.html',
+  styleUrls: ['./policy-page.component.css']
+})
+export class PolicyPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  DEEPSTREAM_URL: 'wss://codecritique.org:6020'
+  origin: 'connectfour.codecritique.org',
+  deepstreamUrl: 'wss://codecritique.org:6020'
 };

src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  DEEPSTREAM_URL: 'ws://localhost:6020'
+  origin: 'localhost',
+  deepstreamUrl: 'ws://localhost:6020'
 };
 
 

src/app/policy-accept/policy-accept.component.spec.ts
@@ -1,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PolicyAcceptComponent } from './policy-accept.component';
+
+describe('PolicyAcceptComponent', () => {
+  let component: PolicyAcceptComponent;
+  let fixture: ComponentFixture<PolicyAcceptComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PolicyAcceptComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PolicyAcceptComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

src/app/policy-page/policy-page.component.spec.ts
@@ -1,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PolicyPageComponent } from './policy-page.component';
+
+describe('PolicyPageComponent', () => {
+  let component: PolicyPageComponent;
+  let fixture: ComponentFixture<PolicyPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PolicyPageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PolicyPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});